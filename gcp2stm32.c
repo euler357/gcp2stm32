1dc62c89574e726aaffbb140f8f75ec0bea6f9eb
euler357 <ckc@accuforge.com>
2022-04-18 16:13:18 -0400

    EXTI now working for Sync

--- gcp2stm32.c
+++ gcp2stm32.c
@@ -8,6 +8,7 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/pwr.h>
 #include <libopencm3/stm32/flash.h>
+#include <libopencm3/stm32/exti.h>
 #include <libopencm3/cm3/assert.h>
 
 #define delay(cycles)       		for (int i = 0; i < cycles; i++) __asm__("nop")
@@ -23,8 +24,12 @@
 #define LED_PORT    GPIOC
 #define LED_PIN     GPIO13
 
-volatile unsigned int adc_buffer0[1000];
-volatile unsigned int adc_buffer1[1000];
+/* Sync Pin */
+#define SYNC_PORT   GPIOA
+#define SYNC_PIN    GPIO11
+
+volatile unsigned int adc_buffer0[256];
+volatile unsigned int adc_buffer1[256];
 
 volatile unsigned int ledcounter=0;
 volatile unsigned int whiteport=0;
@@ -65,7 +70,7 @@ void tim2_isr(void)
                 ((whiteport & 0x1000) >> 12) |  /* A */
                 ((whiteport & 0x0400) >> 9)  |  /* B */
                 ((whiteport & 0x0100) >> 5);    /* D */
-
+#if 0
     if(ledcounter++>500)    
     {
         if(whitebyte & 0x1)
@@ -75,9 +80,12 @@ void tim2_isr(void)
             //gpio_toggle(LED_PORT, LED_PIN); /* LED on/off */
         ledcounter=0;
     }
+#endif
+
     /* Clear Timer 2 Interrupt Flag */
     TIM_SR(TIM2) &= ~TIM_SR_UIF;
 }
+
 
 static void clock_gpio_setup(void)
 {
@@ -86,31 +94,35 @@ static void clock_gpio_setup(void)
     rcc_periph_clock_enable(RCC_GPIOA);
     rcc_periph_clock_enable(RCC_GPIOB);
     rcc_periph_clock_enable(RCC_GPIOC);
+    rcc_periph_clock_enable(RCC_TIM2);      
+    rcc_periph_clock_enable(RCC_ADC);
+    rcc_periph_clock_enable(RCC_USART1);
+    rcc_periph_clock_enable(RCC_SYSCFG);
 
-    gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+    /* Set up Pin Modes */
+    gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, SYNC_PIN);
+    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, \
+        GPIO0 | GPIO1 | GPIO2 | GPIO3 | GPIO4 |  GPIO5 |  GPIO6 |  GPIO7 );
     gpio_mode_setup(GPIOB, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO8 | GPIO9 | \
                      GPIO10 | GPIO11 | GPIO12 | GPIO13 | GPIO14 | GPIO15);
+    gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+    gpio_mode_setup(USART_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, USART_TXPIN);
+    
+    /* Setup USART TX pin as alternate function. */
+    gpio_set_af(USART_PORT, GPIO_AF1, USART_TXPIN);
+
+    gpio_mode_setup(SYNC_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, SYNC_PIN);
 }
 
 static void adc_setup(void)
 {
-    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0);
-    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO1);
-    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO2);
-    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO3);
-    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO4);
-    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO5);
-    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO6);
-    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO7);
-
-    rcc_periph_clock_enable(RCC_ADC);
-
     /* Make sure the ADC doesn't run during config. */
     adc_power_off(ADC1);
 
     /* We configure everything for one single conversion. */
     adc_set_single_conversion_mode(ADC1);
     adc_set_right_aligned(ADC1);
+
     /* We want to read the temperature sensor, so we have to enable it. */
     adc_enable_temperature_sensor();
     adc_set_sample_time_on_all_channels(ADC1, ADC_SMPTIME_039DOT5);
@@ -121,14 +133,6 @@ static void adc_setup(void)
 
 static void usart_setup(void)
 {
-    /* Setup GPIO pins for USART transmit. */
-    gpio_mode_setup(USART_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, USART_TXPIN);
-
-    /* Setup USART TX pin as alternate function. */
-    gpio_set_af(USART_PORT, GPIO_AF0, USART_TXPIN);
-
-    /* Enable clocks for USART. */
-    rcc_periph_clock_enable(RCC_USART1);
 
     /* Setup USART parameters. */
     usart_set_baudrate(USART1, USART_DATARATE);
@@ -142,18 +146,16 @@ static void usart_setup(void)
     usart_enable(USART1);
 }
 
-static void nvic_setup(void)
+static void timer2_setup(void)
 {
     nvic_enable_irq(NVIC_TIM2_IRQ);         /* Enable Timer 2 IRQ */
     nvic_set_priority(NVIC_TIM2_IRQ, 1);    /* Set Timer 2 Int Priority */
-    rcc_periph_clock_enable(RCC_TIM2);      /* Enable clock to Timer 2 */
 
     /* Peripheral Clock = 64MHz */
     /* Set to 10KHz = 0.1mS */
     TIM_CNT(TIM2) = 0;      /* Timer 2 Start Value */
     TIM_PSC(TIM2) = 64;     /* Timer 2 Prescaler */
     TIM_ARR(TIM2) = 100;    /* Timer 2 Max Value */
-    
     TIM_DIER(TIM2) |= TIM_DIER_UIE; /* Enable Timer 2 Interrupt */
     TIM_CR1(TIM2) |= TIM_CR1_CEN;   /* Start Timer 2 */ 
 }
@@ -164,6 +166,27 @@ static void usart_print_string(char *str)
 
     while ((c = *str++) != 0) 
         usart_send_blocking(USART1, c);
+}
+
+/* Handle Sync Pulse */
+void exti4_15_isr(void)
+{
+    exti_reset_request(EXTI11);
+    gpio_toggle(LED_PORT,LED_PIN);
+}
+
+/* Set up interrupt and exti for Sync Pulse */
+static void sync_setup(void)
+{
+
+    nvic_enable_irq(NVIC_EXTI4_15_IRQ);      
+    nvic_set_priority(NVIC_EXTI4_15_IRQ, 2);  
+
+    /* Configure the EXTI subsystem. */
+    exti_select_source(EXTI11, GPIOA);
+    exti_set_trigger(EXTI11, EXTI_TRIGGER_RISING);
+    exti_enable_request(EXTI11);
+
 }
 
 int main(void)
@@ -177,16 +200,11 @@ int main(void)
     /* Wait for power to settle down */
     delay_ms(250);
 
-    /* Set up clocks and GPIO */
     clock_gpio_setup();
-
-    /* Set up USART */
-    usart_setup();
-
-    /* Set up ADC */
     adc_setup();
-	
-    nvic_setup();
+    timer2_setup();
+    usart_setup();
+    sync_setup();
 
     while(1)
 	{
